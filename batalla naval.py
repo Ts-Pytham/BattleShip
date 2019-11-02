d0b01e21c00b8a371b172fef100d1bcea5808502
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2019-11-01 21:58:31 -0500

    BattleShip V.0.0.2
    -ícono.

--- batalla naval.py
+++ batalla naval.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random, os
 import pygame as pg
 
 #COLORES
@@ -17,6 +17,46 @@ var_1 = 0
 var_2 = 0
 # MARGEN entre las cuadriculas
 MARGEN = 2
+
+#----------------------------------------FUNCIONES---------------------------------
+def comprobar_random(valor, modo = 0):
+    global RandomX, RandomY
+    cont = 0
+    pv = True
+    #Este bucle comprueba si hay un barco cerca
+    while True:
+        if modo == 0:
+            if pv:
+                RandomX = random.randint(0, 9)
+                RandomY = random.randint(0, 9-valor)
+                print(f"Valor X: {RandomX}, Valor Y: {RandomY}")
+                pv = False
+
+            if TableroEnemigo[RandomX][RandomY+cont] == 1:
+                RandomX = random.randint(0, 9)
+                RandomY = random.randint(0, 9-valor)
+                print(f"Valor Nuevo de X: {RandomX}, Valor Y: {RandomY}")
+                cont = 0
+            else:
+                cont+=1
+
+        elif modo == 1:
+            if pv:
+                RandomX = random.randint(0, 9-valor)
+                RandomY = random.randint(0, 9)
+                print(f"Valor X: {RandomX}, Valor Y: {RandomY}")
+                pv = False
+                
+            if TableroEnemigo[RandomX+cont][RandomY] == 1:
+                RandomX = random.randint(0, 9-valor)
+                RandomY = random.randint(0, 9)
+                print(f"Nuevo valor de X: {RandomX}, Valor Y: {RandomY}")
+                cont = 0
+            else:
+                cont+=1
+        if cont == valor:
+            break
+
 
 #CREAMOS EL TABLERO DEL JUGADOR
 TableroJugador = []
@@ -67,39 +107,67 @@ contador = 0
 for row in range(1):
     for column in range(2):
         TableroEnemigo[RandomX][RandomY+column] = 1
-#SETEO LAS VARIABLES DE RANDOM
-RandomX = random.randint(0, 8)
-RandomY = random.randint(0, 7)
 
+comprobar_random(2, 1)
 #Hacemos el bucle para la nave  Vertical del enemigo
 for row in range(2):
     for column in range(1):
-        if TableroEnemigo[RandomX+row][RandomY] == 1:
-            if TableroEnemigo[RandomX][RandomY] == 1:
-                TableroEnemigo[RandomX][RandomY] = 0
-            RandomX = random.randint(0, 8)
-            RandomY = random.randint(0, 7)
-            TableroEnemigo[RandomX][RandomY] = 1
-            TableroEnemigo[RandomX+row][RandomY] = 1 
-        else:
             TableroEnemigo[RandomX+row][RandomY] = 1
 
 #---------------------BUCLES DE SUBMARINO (VALOR 3)-------------------------------
+"""
+    Esta funcion busca si en las coordenadas se encuentra un 1, si es así setea 
+    las variables random para comprobar que no haya ningun barco y no se peguen
+    El primer parámetro es la cantidad de iteraciones y la segunda es el modo
+    si es vertical (1) u horizontal (0), aunque si se elimina la segunda variable
+    será 0 por defecto. 
+"""
+comprobar_random(3, 0)
 #Hacemos el bucle para el submarino Horizontal del enemigo.
-NRandomY = random.randint(0, 7)
-NRandomX = random.randint(0, 9)
 for row in range(1):
     for column in range(3):
-        if RandomX == NRandomX and RandomY == NRandomY:
-            NRandomY = random.randint(0, 7)
-            NRandomX = random.randint(0, 9)     
-        TableroEnemigo[NRandomX][NRandomY+column] = 1
+        TableroEnemigo[RandomX][RandomY+column] = 1
+
+#------------------BUCLES DEL CRUCERO O COMO SE LLAME :V (VALOR 4)----------------
+
+comprobar_random(4, 1)
+
+#Hacemos el bucle para el crucero Vertical del enemigo.
+for row in range(4):
+    for column in range(1):
+        TableroEnemigo[RandomX+row][RandomY] = 1
+
+
+#----------------BUCLES DE PORTA-AVIONES (VALOR 5)-------------------------------
+probabilidad = random.randint(0, 1)
+
+comprobar_random(5, probabilidad)
+
+if probabilidad == 0:
+    #El porta-aviones será horizontal
+    for row in range(1):
+        for column in range(5):
+            TableroEnemigo[RandomX][RandomY+column] = 1
+
+else:
+    #El porta-aviones será vertical
+    for row in range(5):
+        for column in range(1):
+            TableroEnemigo[RandomX+row][RandomY] = 1
+
 
 #Imprimimos
 #for i in range(len(TableroJugador)):
  #   print(TableroJugador[i])
 
+#Para encontrar cualquier imagen de manera efizcas.
+current_path = os.path.dirname(__file__) 
+image_path = os.path.join(current_path, 'img')
+
 pg.init()
+
+#Cargar Imágenes
+icon = pygame.image.load(os.path.join(image_path, 'icon.png'))
 
 #DIMENSION DE LA VENTANA
 DIMENSION_VENTANA = [1280, 700]
@@ -107,8 +175,20 @@ ventana = pygame.display.set_mode(DIMENSION_VENTANA)#LE DAMOS DIMENSIONES A LA V
 
 pygame.display.set_caption("BattleShip")
 
-FPS = pygame.time.Clock()
+pygame.display.set_icon(icon)
+
+FPS = pygame.time.Clock() #Se obtiene la velocidad de cuadros
+
+#-----------------------------------FUENTES-----------------------------------------
+arial = pygame.font.SysFont("arial", 30)
+
+#-----------------------------------Textos------------------------------------------
+
+
+#BUCLE PRINCIPAL
 while True:
+    tiempo = int(pygame.time.get_ticks()/1000) #Calcula el tiempo
+
     for evento in pygame.event.get():
         if evento.type == pg.QUIT:
             sys.exit()
@@ -154,6 +234,9 @@ while True:
                                 (MARGEN+ALTO) * fila + MARGEN+50,
                                 BASE,
                                 ALTO])
-            
+    
+
+    T_tiempo = arial.render(f"Tiempo: {tiempo}",0, NEGRO) 
+    ventana.blit(T_tiempo, (0, 0))      
     pygame.display.flip()
-    FPS.tick(60)
+    FPS.tick(60) #Se ajusta la velocidad de fotogramas per segundos a 60
